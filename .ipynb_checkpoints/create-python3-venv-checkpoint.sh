6b5985ed94621027a597fa236fff3ca7f77ffefa
Dietrich Zinsou <dietrich.zinsou@hpe.com>
2020-06-16 14:16:33 +0000

    Remove versioning of temporary files of jupyter

--- .ipynb_checkpoints/create-python3-venv-checkpoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python3 -m venv ~/venv-kaggle-gwd
-pip install pandas jupyter
-ipython kernel install --name "kaggle-gwd" --user
-pip install --upgrade pip
-source ~/venv-kaggle-gwd/bin/activate
